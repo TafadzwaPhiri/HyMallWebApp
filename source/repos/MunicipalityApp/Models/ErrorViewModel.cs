51df20faea00523f5671ea3ab05497becf62bea2
Tafadzwa Phiri <125386750+TafadzwaPhiri@users.noreply.github.com>
2026-03-25 00:51:10 +0200

    Delete source/repos/MunicipalityApp directory

--- source/repos/MunicipalityApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MunicipalityApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- source/repos/MunicipalityApp/Models/EventSearchModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MunicipalityApp.Models
-{
-    public class EventSearchModel
-    {
-        public string Query { get; set; } = "";
-        public List<EventItem> Results { get; set; } = new();
-        public List<EventItem> Recommendations { get; set; } = new();
-    }
-}
